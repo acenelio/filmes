e17258c2eff0ade5a0936199ae60ee0d9190a79a
Nelio Alves DELL <nelio@iftm.edu.br>
2016-02-26 19:39:07 -0300

    Primeiro commit

---
+++ src/main/java/web/ServletTeste.java
@@ -0,0 +1,17 @@
+package web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ServletTeste")
+public class ServletTeste extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+}
